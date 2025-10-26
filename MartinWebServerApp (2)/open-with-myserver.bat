32370b34d436987712fc2d660847921812673a4f
Martin-1-web <martindzeble@gmail.com>
2025-10-26 06:18:48 +0000

    Clean up: Remove duplicate folders and bundled JRE files

--- MartinWebServerApp (2)/open-with-myserver.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set DIR=%~dp0
-"%DIR%customjre\bin\java.exe" -jar "%DIR%ServerDemo.jar" %1
--- MartinWebServerApp/open-with-myserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "$0")" && pwd)"
-"$DIR/customjre/bin/java" -jar "$DIR/ServerDemo.jar" "$1"
